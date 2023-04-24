20d245c07ab7b8535f520a2eb1f8fb08eba06f05
Will-Hoey <will.hoey@hotmail.co.uk>
2023-04-24 12:33:26 +0100

    feat: script to autogenerate changelogs
    - takes N number of commits and writes them to CHANGELOG

---
+++ generate_changelog.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Get the remote repository URL
+remote_url=$(git config --get remote.origin.url | tr -d '\n')
+
+# Get the last n commits
+n=$1
+commits=$(git log -n $n --pretty=format:'%H %s')
+
+# Build the changelog
+changelog="## Changelog\n\n"
+while read -r commit; do
+  sha=$(echo "$commit" | cut -d ' ' -f 1)
+  message=$(echo "$commit" | cut -d ' ' -f 2-)
+  changelog+="- $message ([${sha:0:7}](${remote_url}/commit/${sha}))\n"
+done <<< "$commits"
+
+# Open or create the CHANGELOG file
+if [ -f "CHANGELOG" ]; then
+  contents=$(cat CHANGELOG)
+  echo -e "$changelog\n$contents" > CHANGELOG
+else
+  echo -e "$changelog" > CHANGELOG
+fi

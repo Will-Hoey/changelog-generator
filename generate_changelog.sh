2818433c735f47d14f996a0c8e7dabe72b401e52
Jon Atkinson <jon@jonatkinson.co.uk>
2023-05-03 11:12:33 +0100

    Revert "feat: Allow the user to specify the output file, and by default output to stdout."
    This reverts commit 9c6799e7b982ba8eebe93d3867034b84d6f6ae04.

--- generate_changelog.sh
+++ generate_changelog.sh
@@ -1,34 +1,13 @@
 #!/bin/bash
 
-# Function to display usage instructions
-function usage() {
-  echo "Usage: $0 [-n num_commits] [-o output_file]"
-  exit 1
-}
-
-# Parse command line options
-while getopts "n:o:" opt; do
-  case $opt in
-    n)
-      num_commits="$OPTARG"
-      ;;
-    o)
-      output_file="$OPTARG"
-      ;;
-    *)
-      usage
-      ;;
-  esac
-done
-
 # Get the remote repository URL
 remote_url=$(git config --get remote.origin.url | tr -d '\n')
 
-# Get the last n commits or all since
-if [ -z "$num_commits" ]; then
+# Get the last n commits or all since 
+if [ -z "$1" ]; then
   commits=$(git log --no-merges --invert-grep --grep='^style:' --pretty=format:'%H %s' $(git log -n 1 origin/develop --pretty=format:'%H')..HEAD)
 else
-  commits=$(git log --no-merges --invert-grep --grep='^style:' -n $num_commits --pretty=format:'%H %s')
+  commits=$(git log --no-merges --invert-grep --grep='^style:' -n $1 --pretty=format:'%H %s')
 fi
 
 # Build the changelog
@@ -39,14 +18,10 @@ while read -r commit; do
   changelog+="- $message ([${sha:0:7}](${remote_url}/commit/${sha}))\n"
 done <<< "$commits"
 
-# Output the changelog
-if [ -z "$output_file" ]; then
-  echo -e "$changelog"
+# Open or create the CHANGELOG file
+if [ -f "CHANGELOG" ]; then
+  contents=$(cat CHANGELOG)
+  echo -e "$changelog\n$contents" > CHANGELOG
 else
-  if [ -f "$output_file" ]; then
-    contents=$(cat "$output_file")
-    echo -e "$changelog\n$contents" > "$output_file"
-  else
-    echo -e "$changelog" > "$output_file"
-  fi
+  echo -e "$changelog" > CHANGELOG
 fi
